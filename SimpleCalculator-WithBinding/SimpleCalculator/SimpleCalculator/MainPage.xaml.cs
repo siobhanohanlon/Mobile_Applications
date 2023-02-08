b069eae49bb336d9822813261e6a627a2fd39411
Siobhán O'Hanlon <80902477+siobhanohanlon@users.noreply.github.com>
2023-02-08 13:07:59 +0000

    Simple Binding Calculator

---
+++ SimpleCalculator-WithBinding/SimpleCalculator/SimpleCalculator/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SimpleCalculator
+{
+    public partial class MainPage : ContentPage
+    {
+        Calculate calc;
+
+        public MainPage()
+        {
+            //Initialize
+            InitializeComponent();
+
+            //If calc is nothing
+            if(calc == null)
+            {
+                calc = new Calculate();
+            }
+
+            //Binding
+            this.BindingContext = calc;
+        }
+
+        private void BtnDoMaths_Clicked(object sender, EventArgs e)
+        {
+            calc.Arg1 = 27;
+            calc.Arg2 = 15;
+        }
+    }
+}
